446083feb94b0e703d272d8a8e1529acafb081f9
ghorsey <geoff@shhhinnovations.com>
2013-04-02 12:23:47 -0700

    added nuget push artifacts

--- code/src/SHHH.Infrastructure/Properties/AssemblyInfo.cs
+++ code/src/SHHH.Infrastructure/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SHHH.Infrastructure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base classes for setting up a domain model")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("SHHH Innovations LLC")]
 [assembly: AssemblyProduct("SHHH.Infrastructure")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © SHHH Innovation LLC 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
