150899f50eb26f7492a7110eef81e4506be56704
ghorsey <geoff@shhhinnovations.com>
2013-04-09 01:05:30 -0700

    marked the assembly to allow partially trusted callers

--- code/src/SHHH.Infrastructure/Properties/AssemblyInfo.cs
+++ code/src/SHHH.Infrastructure/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -18,11 +19,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("959edaf4-06c7-4b5a-9894-f13ea8faafc4")]
 
-[assembly: AssemblyVersion("2.2.2.4")]
-[assembly: AssemblyFileVersion("2.2.2.4")]
+[assembly: AssemblyVersion("2.2.2.5")]
+[assembly: AssemblyFileVersion("2.2.2.5")]
